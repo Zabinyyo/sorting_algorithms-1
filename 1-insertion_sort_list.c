66c7cdb6d4680d321ed9a97d8310a833e43b565a
1uiscalderon <1857@holbertonschool.com>
2020-10-16 14:53:12 -0500

    Insertion sort algorith

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,61 @@
+#include "sort.h"
+void swap_head(listint_t **list, listint_t *aux)
+{
+	aux->prev->next = aux->next;
+	aux->next->prev = aux->prev;
+	aux->next = aux->prev;
+	aux->prev = aux->prev->prev;
+	aux->next->prev = aux;
+	*list = aux;
+}
+void swap_middle(listint_t *aux)
+{
+	aux->prev->next = aux->next;
+	aux->next->prev = aux->prev;
+	aux->prev->prev->next = aux;
+	aux->next = aux->prev;
+	aux->prev = aux->next->prev;
+	aux->next->prev = aux;
+}
+void swap_tail(listint_t *aux)
+{
+	aux->prev->next = aux->next;
+	aux->next = aux->prev;
+	aux->prev->prev->next = aux;
+	aux->prev = aux->next->prev;
+	aux->next->prev = aux;
+}
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *aux = NULL;
+	int i = 0, j = 0;
+
+	if (!list || !(*list)->next)
+		return;
+	aux = (*list)->next;
+	while (aux)
+	{
+		i++;
+		while (aux->prev)
+		{
+			if (aux->prev->n > aux->n)
+			{
+				if (!aux->prev->prev)
+					swap_head(list, aux);
+				else if (aux->prev->prev && aux->next)
+					swap_middle(aux);
+				else if (!aux->next)
+					swap_tail(aux);
+				print_list(*list);
+			}
+			else
+				aux = aux->prev;
+		}
+		while (j <= i)
+		{
+			aux = aux->next;
+			j++;
+		}
+		j = 0;
+	}
+}
