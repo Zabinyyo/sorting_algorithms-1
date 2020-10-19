030e23c9e891c1b16a814294dca6fc0aa2ba825f
1uiscalderon <1857@holbertonschool.com>
2020-10-19 10:08:21 -0500

    Betty check

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,13 @@
 #include "sort.h"
-
+/**
+ * selection_sort - This algorithm will first find the smallest
+ * element in the array and swap it with the element in the first position,
+ * then it will find the second smallest element and swap it with the element
+ * in the second position, and it will keep on doing this until the entire
+ * array is sorted.
+ * @array: Array of data to be sorted
+ * @size: Size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
 	size_t i = 0, j = 0, index = 0;
