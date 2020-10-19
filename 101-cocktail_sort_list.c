3d1cc24000646a0aef9231b386d62dae2ede1f16
1uiscalderon <1857@holbertonschool.com>
2020-10-19 18:29:58 -0500

    Betty check

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -39,7 +39,11 @@ void swap_tail(listint_t *aux)
 	aux->prev = aux->next->prev;
 	aux->next->prev = aux;
 }
-
+/**
+ * evaluate_swap - checks the position to do the swap
+ * @list: Doubly linked list with nodes to sort acording to number n.
+ * @aux: auxiliar node to compare
+ */
 void evaluate_swap(listint_t **list, listint_t *aux)
 {
 	if (!aux->prev->prev)
@@ -49,7 +53,14 @@ void evaluate_swap(listint_t **list, listint_t *aux)
 	else if (!aux->next)
 		swap_tail(aux);
 }
-
+/**
+ * cocktail_sort_list - Cocktail Sort is a variation of Bubble sort.
+ * The Bubble sort algorithm always traverses elements from left and
+ * moves the largest element to its correct position in first iteration
+ * and second largest in second iteration and so on. Cocktail Sort traverses
+ * through a given array in both directions alternatively.
+ * @list: Doubly linked list with nodes to sort acording to number n.
+ */
 void cocktail_sort_list(listint_t **list)
 {
 	listint_t *aux = NULL, *tmp;
@@ -63,7 +74,7 @@ void cocktail_sort_list(listint_t **list)
 		aux = tmp, swap_flag = 0;
 		while (aux)
 		{
-			if (aux->n < aux->prev->n)
+			if (aux->prev && aux->n < aux->prev->n)
 			{
 				evaluate_swap(list, aux);
 				print_list(*list), swap_flag = 1;
@@ -76,7 +87,7 @@ void cocktail_sort_list(listint_t **list)
 		aux = aux->prev;
 		while (aux->prev)
 		{
-			if (aux->prev->n > aux->n)
+			if (aux->prev && aux->prev->n > aux->n)
 			{
 				evaluate_swap(list, aux);
 				print_list(*list), swap_flag = 1;
