51df1380c802292117f2d8c15be47da35bdac378
oimoralest <oimoralest@gmail.com>
2020-10-19 10:40:34 -0500

    Header updatep

--- sort.h
+++ sort.h
@@ -34,5 +34,7 @@ void swap_middle(listint_t *aux);
 void swap_tail(listint_t *aux);
 int partition(int *array, int low, int high, size_t size);
 void quick(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
+int pow_(int base, int power);
 
 #endif
