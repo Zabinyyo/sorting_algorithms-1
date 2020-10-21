8bc426ef5853d4fb4c5722cc2b466d239592d708
oimoralest <oimoralest@gmail.com>
2020-10-21 16:07:15 -0500

    Modified

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -88,7 +88,11 @@ void sort_(int *array, size_t low, size_t size, size_t dir, size_t length)
  */
 void bitonic_sort(int *array, size_t size)
 {
-	if (size < 2 || size % 2 != 0)
+	size_t i = 1;
+
+	while (i < size)
+		i <<= 1;
+	if (size < 2 || (i ^ size) != 0)
 		return;
 	printf("Mergin [%li/%li] (UP):\n", size, size);
 	print_array(array, size);
